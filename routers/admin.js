8479b8b4da2a4b65c4706c70762595c5697ea218
小海 <1378465762@qq.com>
2019-02-03 17:38:37 +0800

    Update admin.js

--- routers/admin.js
+++ routers/admin.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-router.get('/user',function(req,res,next){
-    res.send('User');
+router.get('/',function(req,res,next){
+    // res.send('后台管理');
+    res.redirect('http://localhost:8080/dist/about/about.html')
+   
 })
 module.exports = router;
