83043cfbe4f1672fe7a0f2b0fe29e64a911a0a76
小海 <1378465762@qq.com>
2019-02-13 10:20:18 +0800

    Update admin.js

--- routers/admin.js
+++ routers/admin.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
 router.get('/',function(req,res,next){
-    // res.send('后台管理');
+   
     res.redirect('http://localhost:8080/dist/about/about.html')
    
 })
+router.use(function(req,res,next){
+    Responsedata = {
+        code:0,//无错
+        message:''
+    }
+    next();
+})
+User.find().then(function(users){
+    router.post('/user',function(req,res){
+        console.log(req.body);
+        Responsedata.message = users;
+        res.json(Responsedata);
+    })
+})
+
 module.exports = router;
