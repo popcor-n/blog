251ae10bb25f7901ad1bcc55c541d7ed4d265b30
小海 <1378465762@qq.com>
2019-02-24 17:35:34 +0800

    Add files via upload

---
+++ schema/content.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+//定义 内容结构
+module.exports =  new  mongoose.Schema({
+
+    //关联字段：
+    classify:{
+        //类型
+        type:mongoose.Schema.Types.ObjectId,
+        //引用
+        ref:'Content'
+    },
+    //分类名
+    title:String,
+    //内容
+    content:{
+        type:String,
+        default:''
+    }
+})
