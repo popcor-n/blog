2b520fc107663ff648a760f35ac981b4b7a86a23
小海 <1378465762@qq.com>
2019-03-02 14:59:01 +0800

    Update main.js

--- routers/main.js
+++ routers/main.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var Classify = require('../models/Classify');
+var Content = require('../models/Content');
+
 router.use(function(req,res,next){
     Responsedata = {
         code:0,//无错
@@ -8,10 +10,26 @@ router.use(function(req,res,next){
     }
     next();
 })
+
 router.post('/nav',function(req,res){
     Classify.find().then(function(classify){
-        // console.log(req.body);
+    // console.log(req.body);
         Responsedata.message = classify;
+        res.json(Responsedata);
+    })
+})
+router.post('/content',function(req,res){
+    Content.find().sort({_id:-1}).then(function(con){
+    // console.log(req.body);
+        Responsedata.message = con;
+        res.json(Responsedata);
+    })
+})
+router.post('/content/class',function(req,res){
+    console.log(req.body.req);
+    Content.find({classify:req.body.req}).sort({_id:-1}).then(function(con){
+    // console.log(req.body);
+        Responsedata.message = con;
         res.json(Responsedata);
     })
 })
